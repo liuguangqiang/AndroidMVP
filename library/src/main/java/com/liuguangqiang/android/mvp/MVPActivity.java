76349e6d4cb88dea17744b0def61fef36dd228a3
Eric <guangqiang.dev@gmail.com>
2015-05-11 17:05:40 +0800

    Create MVPActivity for using MVP more easy.

---
+++ library/src/main/java/com/liuguangqiang/android/mvp/MVPActivity.java
@@ -0,0 +1,44 @@
+package com.liuguangqiang.android.mvp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Eric on 15/5/11.
+ */
+public class MVPActivity extends ActionBarActivity {
+
+    private Presenter presenter;
+    private BaseUi baseUi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        presenter = setPresenter();
+        baseUi = setUi();
+    }
+
+    public Presenter setPresenter() {
+        return null;
+    }
+
+    public BaseUi setUi() {
+        return null;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (presenter != null && baseUi != null)
+            presenter.attach(baseUi);
+    }
+
+    @Override
+    protected void onPause() {
+        if (presenter != null && baseUi != null)
+            presenter.detach(baseUi);
+        super.onPause();
+    }
+
+
+}
